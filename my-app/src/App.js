388d45fd4edcc2421290ba5cf2f9edb7e1c2f5e6
Maalik McBurrows <mmcburrows1@gmail.com>
2020-03-24 14:56:13 -0400

    refactored the component props to state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,12 @@ import Quote from './components/quote'
 import './App.css';
 
 function App() {
-  const thisQuote = "Stay in the crib :("
   return (
     <div className="App">
       <header className="App-header">
         <h1>Chuck Says...</h1>
       </header>
-      <Quote quote={ thisQuote } />
+      <Quote />
     </div>
   );
 }
--- my-app/src/components/quote.jsx
+++ my-app/src/components/quote.jsx
@@ -1,8 +1,11 @@
 import React, {Component } from 'react';
 
 class Quote extends Component {
+    state = {
+        quote: "Chuck norris made corona stay inside."
+    }
     render() {
-        const { quote } = this.props;
+        const { quote } = this.state;
         return (
             <p>
                 {quote}
