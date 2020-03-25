2cd0622834945cfc1ba8df2ddf79fc78420bd056
Maalik McBurrows <mmcburrows1@gmail.com>
2020-03-25 02:11:03 -0400

    added componentDidMount method to my quote file

--- my-app/src/components/quote.jsx
+++ my-app/src/components/quote.jsx
@@ -2,8 +2,17 @@ import React, {Component } from 'react';
 
 class Quote extends Component {
     state = {
-        quote: "Chuck norris made corona stay inside."
+        quote: "Getting quote from API"
     }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                quote: "Chuck norris made corona stay inside."
+            })
+        }, 2000);
+    }
+
     render() {
         const { quote } = this.state;
         return (
